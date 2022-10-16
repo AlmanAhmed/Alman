3ce6cb4031fce3ed4bfb7ed05b4bd11ecfa6ed2c
Alman <107334202+AlmanAhmed@users.noreply.github.com>
2022-10-16 10:59:50 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -35,13 +35,14 @@ entries.forEach(entry => {
 }, sectionOneOptions)
 sectionOneObserver.observe(sectionOne)
 
-//loading screen
 
+//loading screen
 var loader = document.getElementById("loading-scr");
 
 window.addEventListener("load", function(){
     loader.style.display = "none";
 })
+
 
 // stripe design
 var c = document.getElementById('canv');
@@ -79,3 +80,51 @@ var run = function() {
 }
 
 run();
+
+
+//rotating text
+"use strict";
+
+let words = document.querySelectorAll(".word");
+
+words.forEach((word) => {
+  let letters = word.textContent.split("");
+  word.textContent = "";
+  letters.forEach((letter) => {
+    let span =document.createElement("span");
+    span.textContent = letter;
+    span.className = "letter";
+    word.append(span);
+  });
+});
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length -1;
+words[currentWordIndex].style.opacity = "1";
+
+let rotateText = () => {
+  let currentWord = words[currentWordIndex];
+  let nextWord =
+  currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+  // rotate out letters off current words
+  Array.from(currentWord.children).forEach((letter, i) => {
+    setTimeout(()=> {
+      letter.className = "letter out"
+    }, i*80);
+  });
+
+  //reveal and rotate next word
+  nextWord.style.opacity = "1";
+  Array.from(nextWord.children).forEach((letter, i) => {
+    letter.className = "letter behind";
+    setTimeout(() => {
+      letter.className = "letter in";
+    }, 340 + i * 80);
+  });
+  currentWordIndex =
+    currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+
+};
+
+rotateText();
+setInterval(rotateText, 4000);
